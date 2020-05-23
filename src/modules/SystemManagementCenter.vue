2ea1d9f5575976f5ad26cf914590430cc8928d1c
gjp <gjp@winning.com.cn>
2020-05-23 11:48:43 +0800

    模糊查询

--- src/modules/SystemManagementCenter.vue
+++ src/modules/SystemManagementCenter.vue
@@ -1,10 +1,7 @@
 <template>
   <el-row style="height: 100%;">
-    <el-col :span="6">
-<!--      <h5>自定义颜色</h5>-->
-      <!--  default-active="2"  -->
+    <el-col :span="6" style="height: 100%;background: #545c64">
       <el-menu
-        class="el-menu-vertical-demo"
         @open="handleOpen"
         @close="handleClose"
         @select="handleSelect"
@@ -25,11 +22,24 @@
     </el-col>
     <el-col :span="18"  style="height: 100%">
       <div v-show="isShow11" style="height: 100%">
+        <el-form :model="params" style="font: 12px/1.5 Tahoma,Helvetica,Arial,'宋体',sans-serif;">
+          <el-select v-model="params.siteName" size="small" plach>
+            <el-option
+              v-for="item in siteList"
+              :key="item.siteId"
+              :label="item.siteName"
+              :value="item.siteId"
+            >
+            </el-option>
+          </el-select>
+          页面别名: <el-input v-model="params.pageAlias" style="width: 10%" size="small"></el-input>
+          <el-button @click="queryAllCmsPageList" size="small">查询</el-button>
+        </el-form>
         <el-table
           :data="tableData"
           style="width: 100%;border: 1px solid cyan"
           height="90%"
-          stripe="true"
+          :stripe="true"
           size="small"
           :row-class-name="tableRowClassName">
           <el-table-column
@@ -39,15 +49,18 @@
           </el-table-column>
           <el-table-column
             prop="pageAlias"
-            label="页面别名">
+            label="页面别名"
+            :show-overflow-tooltip="true">
           </el-table-column>
           <el-table-column
             prop="pageType"
-            label="页面类型">
+            label="页面类型"
+            :show-overflow-tooltip="true">
           </el-table-column>
           <el-table-column
             prop="pageWebPath"
-            label="访问路径">
+            label="访问路径"
+            :show-overflow-tooltip="true">
           </el-table-column>
           <el-table-column
             prop="pagePhysicalPath"
@@ -77,6 +90,11 @@ export default {
   name: 'SystemManagementCenter',
   data () {
     return {
+      siteList: [],
+      params: {
+        'siteId': '',
+        'pageAlias': ''
+      },
       isShow11: false,
       tableData: [],
       pageNum: 1,
@@ -85,15 +103,33 @@ export default {
     }
   },
   created () {
+    this.queryAllCmsSiteList()
     this.queryAllCmsPageList()
   },
   methods: {
     queryAllCmsPageList () {
       const _this = this
-      axios.get('/api/cms/page/queryAllCmsPageList/' + _this.pageNum + '/' + _this.pageSize)
+      axios.get('/api/cms/page/queryAllCmsPageList/' + _this.pageNum + '/' + _this.pageSize,
+        {
+          params: {
+            siteId: _this.params.siteId,
+            pageAlias: _this.params.pageAlias
+          }
+        })
         .then(function (response) {
           _this.tableData = response.data.queryResult.list
           _this.total = response.data.queryResult.total
+        })
+        .catch(function (error) {
+          _this.$message.error(error.toString())
+        })
+    },
+    async queryAllCmsSiteList () {
+      const _this = this
+      await axios.get('/api/cms/site/queryAllCmsSiteList/')
+        .then(function (response) {
+          _this.siteList = response.data.queryResult.list
+          console.log(JSON.stringify(_this.siteList))
         })
         .catch(function (error) {
           _this.$message.error(error.toString())
