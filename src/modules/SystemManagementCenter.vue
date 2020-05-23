78057d89e4776506fb738866b6ce876f69464c1b
gjp <gjp@winning.com.cn>
2020-05-23 16:52:16 +0800

    模糊查询

--- src/modules/SystemManagementCenter.vue
+++ src/modules/SystemManagementCenter.vue
@@ -34,6 +34,7 @@
           </el-select>
           页面别名: <el-input v-model="params.pageAlias" style="width: 10%" size="small"></el-input>
           <el-button @click="queryAllCmsPageList" size="small">查询</el-button>
+          <el-button @click="dialogFormVisible11=true" size="small">新增页面</el-button>
         </el-form>
         <el-table
           :data="tableData"
@@ -67,6 +68,15 @@
             label="物理路径"
             :show-overflow-tooltip="true">
           </el-table-column>
+          <el-table-column
+            fixed="right"
+            label="操作"
+            width="100">
+            <template slot-scope="scope">
+              <el-button @click="handleClickForUpdate(scope.row)" type="text" size="small">修改</el-button>
+              <el-button @click="handleClickForDelete(scope.row)" type="text" size="small">删除</el-button>
+            </template>
+          </el-table-column>
         </el-table>
         <div class="block">
           <el-pagination
@@ -79,6 +89,116 @@
             :total="total">
           </el-pagination>
         </div>
+        <el-dialog title="添加页面" :visible.sync="dialogFormVisible11" size="small" @close="dialogFormClose()">
+          <el-form :model="dialogForm11" size="small">
+            <el-form-item label="所属站点" :label-width="dialogFormLabelWidth11">
+              <el-select v-model="dialogForm11.siteId" placeholder="请选择活动区域">
+                <el-option
+                  v-for="item in siteList"
+                  :key="item.siteId"
+                  :label="item.siteName"
+                  :value="item.siteId"
+                >
+                </el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="选择模板" :label-width="dialogFormLabelWidth11">
+              <el-select v-model="dialogForm11.templateId" placeholder="请选择活动区域">
+                <el-option
+                  v-for="item in templateList"
+                  :key="item.templateId"
+                  :label="item.templateName"
+                  :value="item.templateId"
+                >
+                </el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="页面名称" :label-width="dialogFormLabelWidth11">
+              <el-input v-model="dialogForm11.pageName" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="别名" :label-width="dialogFormLabelWidth11">
+              <el-input v-model="dialogForm11.pageAlias" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="访问路径" :label-width="dialogFormLabelWidth11">
+              <el-input v-model="dialogForm11.pageWebPath" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="物理路径" :label-width="dialogFormLabelWidth11">
+              <el-input v-model="dialogForm11.pagePhysicalPath" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="类型" :label-width="dialogFormLabelWidth11">
+              <el-radio-group v-model="dialogForm11.pageType">
+                <el-radio :label="0">静态</el-radio>
+                <el-radio :label="1">动态</el-radio>
+              </el-radio-group>
+            </el-form-item>
+            <el-form-item label="创建时间" :label-width="dialogFormLabelWidth11">
+              <el-date-picker
+                v-model="dialogForm11.pageCreateTime"
+                type="date"
+                placeholder="选择日期">
+              </el-date-picker>
+            </el-form-item>
+          </el-form>
+          <div slot="footer" class="dialog-footer">
+            <el-button @click="dialogFormClose()" size="small">返 回</el-button>
+            <el-button type="primary" @click="dialogFormSubmit()" size="small">提 交</el-button>
+          </div>
+        </el-dialog>
+        <el-dialog title="修改页面" :visible.sync="dialogFormVisible111" size="small" @close="dialogForm111Close()">
+          <el-form :model="dialogForm111" size="small">
+            <el-form-item label="所属站点" :label-width="dialogFormLabelWidth111">
+              <el-select v-model="dialogForm111.siteId" placeholder="请选择活动区域">
+                <el-option
+                  v-for="item in siteList"
+                  :key="item.siteId"
+                  :label="item.siteName"
+                  :value="item.siteId"
+                >
+                </el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="选择模板" :label-width="dialogFormLabelWidth111">
+              <el-select v-model="dialogForm111.templateId" placeholder="请选择活动区域">
+                <el-option
+                  v-for="item in templateList"
+                  :key="item.templateId"
+                  :label="item.templateName"
+                  :value="item.templateId"
+                >
+                </el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="页面名称" :label-width="dialogFormLabelWidth111">
+              <el-input v-model="dialogForm111.pageName" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="别名" :label-width="dialogFormLabelWidth111">
+              <el-input v-model="dialogForm111.pageAlias" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="访问路径" :label-width="dialogFormLabelWidth111">
+              <el-input v-model="dialogForm111.pageWebPath" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="物理路径" :label-width="dialogFormLabelWidth111">
+              <el-input v-model="dialogForm111.pagePhysicalPath" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="类型" :label-width="dialogFormLabelWidth111">
+              <el-radio-group v-model="dialogForm111.pageType">
+                <el-radio label="0">静态</el-radio>
+                <el-radio label="1">动态</el-radio>
+              </el-radio-group>
+            </el-form-item>
+            <el-form-item label="创建时间" :label-width="dialogFormLabelWidth111">
+              <el-date-picker
+                v-model="dialogForm111.pageCreateTime"
+                type="date"
+                placeholder="选择日期">
+              </el-date-picker>
+            </el-form-item>
+          </el-form>
+          <div slot="footer" class="dialog-footer">
+            <el-button @click="dialogForm111Close()" size="small">返 回</el-button>
+            <el-button type="primary" @click="dialogForm111Submit()" size="small">提 交</el-button>
+          </div>
+        </el-dialog>
       </div>
     </el-col>
   </el-row>
@@ -91,6 +211,7 @@ export default {
   data () {
     return {
       siteList: [],
+      templateList: [],
       params: {
         'siteId': '',
         'pageAlias': ''
@@ -99,14 +220,101 @@ export default {
       tableData: [],
       pageNum: 1,
       pageSize: 10,
-      total: 100
+      total: 100,
+      dialogFormVisible11: false,
+      dialogFormVisible111: false,
+      dialogForm11: {
+        'siteId': '',
+        'pageName': '',
+        'pageAlias': '',
+        'pageWebPath': '',
+        'pagePhysicalPath': '',
+        'pageType': '',
+        'pageCreateTime': null,
+        'templateId': '',
+        'htmlFileId': ''
+      },
+      dialogForm111: {
+        'siteId': '',
+        'pageName': '',
+        'pageAlias': '',
+        'pageWebPath': '',
+        'pagePhysicalPath': '',
+        'pageType': '',
+        'pageCreateTime': null,
+        'templateId': '',
+        'htmlFileId': ''
+      },
+      dialogFormLabelWidth11: '120px',
+      dialogFormLabelWidth111: '120px'
     }
   },
   created () {
     this.queryAllCmsSiteList()
+    this.queryAllCmsTemplateList()
     this.queryAllCmsPageList()
   },
   methods: {
+    dialogFormSubmit () {
+      const _this = this
+      axios.post('/api/cms/page/insertCmsPage/', _this.dialogForm11
+      ).then(function (response) {
+        console.log(JSON.stringify(response))
+        if (response.data.success) {
+          _this.$message.success('添加成功')
+        } else {
+          _this.$message.warning('该页面已存在')
+        }
+      }).catch(function (error) {
+        _this.$message.error(error.toString())
+      })
+      this.dialogFormClose()
+    },
+    dialogFormClose () {
+      this.dialogForm11 = {'siteId': '', 'pageName': '', 'pageAlias': '', 'pageWebPath': '', 'pagePhysicalPath': '', 'pageType': '', 'pageCreateTime': null, 'templateId': '', 'htmlFileId': ''}
+      this.dialogFormVisible11 = false
+      this.queryAllCmsPageList()
+    },
+    /** 主页面触发修改页面的函数 */
+    async handleClickForDelete (row) {
+      const _this = this
+      await axios.post('/api/cms/page/deleteCmsPageById/', row
+      ).then(function (response) {
+        console.log(JSON.stringify(response))
+        if (response.data.success) {
+          _this.$message.success('删除成功')
+        }
+      }).catch(function (error) {
+        _this.$message.error(error.toString())
+      })
+      this.queryAllCmsPageList()
+    },
+    /** 主页面触发修改页面的函数 */
+    handleClickForUpdate (row) {
+      this.dialogFormVisible111 = true
+      this.dialogForm111 = row
+    },
+    /** 修改页面的提交方法 */
+    dialogForm111Submit () {
+      const _this = this
+      axios.post('/api/cms/page/updateCmsPage/', _this.dialogForm111
+      ).then(function (response) {
+        console.log(JSON.stringify(response))
+        if (response.data.success) {
+          _this.$message.success('修改成功')
+        } else {
+          _this.$message.warning('该页面已存在')
+        }
+      }).catch(function (error) {
+        _this.$message.error(error.toString())
+      })
+      this.dialogForm111Close()
+    },
+    dialogForm111Close () {
+      this.dialogForm111 = {'siteId': '', 'pageName': '', 'pageAlias': '', 'pageWebPath': '', 'pagePhysicalPath': '', 'pageType': '', 'pageCreateTime': null, 'templateId': '', 'htmlFileId': ''}
+      this.dialogFormVisible111 = false
+      this.queryAllCmsPageList()
+    },
     queryAllCmsPageList () {
       const _this = this
       axios.get('/api/cms/page/queryAllCmsPageList/' + _this.pageNum + '/' + _this.pageSize,
@@ -130,6 +338,17 @@ export default {
         .then(function (response) {
           _this.siteList = response.data.queryResult.list
           console.log(JSON.stringify(_this.siteList))
+        })
+        .catch(function (error) {
+          _this.$message.error(error.toString())
+        })
+    },
+    async queryAllCmsTemplateList () {
+      const _this = this
+      await axios.get('/api/cms/template/queryAllCmsTemplateList/')
+        .then(function (response) {
+          _this.templateList = response.data.queryResult.list
+          console.log(JSON.stringify(_this.templateList))
         })
         .catch(function (error) {
           _this.$message.error(error.toString())
